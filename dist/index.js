a8592d60922d80c36621208eba25d2f0e64d15a1
zhangshuai <shuai.zhang@qihaixing.onaliyun.com>
2020-09-22 11:39:45 +0800

    new url

--- dist/index.js
+++ dist/index.js
@@ -258,7 +258,7 @@ function getSong() {
     audio = document.getElementById("audio");
     audio.loop = true; //歌曲循环
     playCotrol(); //播放控制函数
-    audio.src = "https://sharefs.yun.kugou.com/202009160004/645896571d49f0cf1d8295976813735f/G126/M0A/06/07/HocBAFqWqw6AKxmcADX3sA0k39o695.mp3"
+    audio.src = "https://webfs.yun.kugou.com/202009221137/313e8c01fcbd0262e5209df8a483366d/G126/M0A/06/07/HocBAFqWqw6AKxmcADX3sA0k39o695.mp3"
 }
 
 function start() {
